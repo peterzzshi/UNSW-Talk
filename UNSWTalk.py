b422957afd6fada0afa8eb1373c9443eeaed1834
Peter Shi <peterzzshi@gmail.com>
2018-11-29 00:11:56 +1100

    testing

---
+++ UNSWTalk.py
@@ -0,0 +1,87 @@
+from app import app
+
+# from flask import Flask, render_template, session, request, flash, redirect, url_for
+# from pymongo import MongoClient
+#
+# from functools import wraps
+#
+# from wtforms import Form, BooleanField, StringField, PasswordField, validators
+#
+# class RegistrationForm(Form):
+#     username = StringField('Username', [validators.Length(min=4, max=25)])
+#     email = StringField('Email Address', [validators.Length(min=6, max=35)])
+#     password = PasswordField('New Password', [
+#         validators.DataRequired(),
+#         validators.EqualTo('confirm', message='Passwords must match')
+#     ])
+#     confirm = PasswordField('Repeat Password')
+#     accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+#
+#
+#
+#
+#
+# client = MongoClient('localhost', 27017)
+# db = client["UNSW-Talk"]
+#
+# students = db["students"]
+# posts = db["posts"]
+#
+#
+# # print(students.find_one({"_id": "z5190454"}))
+# print([student for student in students.find({})])
+#
+# app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'you-will-never-guess'
+#
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+#
+#
+#
+#
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     form = RegistrationForm(request.form)
+#     if request.method == 'POST' and form.validate():
+#         user = User(form.username.data, form.email.data,
+#                     form.password.data)
+#         # db_session.add(user)
+#         flash('Thanks for registering')
+#         return redirect(url_for('login'))
+#     return render_template('register.html', form=form)
+#
+#
+#
+#
+# @app.route('/students')
+# def students():
+#     return [student for student in students.find({})]
+#
+#
+# @app.route('/students/<zid>')
+# def student(zid):
+#     return students.find_one({"_id": zid})
+#
+#
+#
+# # @app.route('/posts')
+# # def posts():
+# #     return posts.find({})
+#
+#
+# @app.route('/posts/<zid>')
+# def posts(zid):
+#     return posts.find_one({"_id": zid})
+#
+#
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True, use_reloader=True)
+#
+#
+#
+#
---
+++ mongo.py
@@ -0,0 +1,107 @@
+import os
+from itertools import groupby
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client["UNSW-Talk"]
+
+students = db["students"]
+posts = db["posts"]
+
+students_dir = "dataset-medium"
+
+
+def read_student_information(zid):
+    # returns information for one student
+    details_filename = os.path.join(students_dir, zid, "student.txt")
+    image_path = os.path.join(students_dir, zid, "img.jpg")
+
+    with open(details_filename) as f:
+        fields = [line.strip().split(':') for line in f]
+
+    student_information = {}
+    for field in fields:
+        if field[0] == "friends" or field[0] == "courses":
+            student_information[field[0]] = field[1].strip()[1:-2].split(', ')
+        else:
+            student_information[field[0]] = field[1].strip()
+    student_information['image'] = image_path
+    student_information['_id'] = zid
+    return student_information
+
+
+def read_student_posts(zid):
+    # returns posts for one student
+    foldername = os.path.join(students_dir, zid)
+    filenames = sorted(list(filter(lambda x: x[0].isdigit(), os.listdir(foldername))))
+
+    posts_filenames_groups = [list(g) for k, g in groupby(filenames, key=lambda x: x[0])]
+
+    def to_tuple(name):
+        """Extract all numbers from file as tuple (8,1,3) ... etc."""
+        return tuple(map(int, name.split(".")[0].split("-")))
+
+    posts_information = {
+        '_id': zid
+    }
+
+    posts = []
+    
+    for posts_filenames in posts_filenames_groups:
+        # sort the files in order of post, comment, replies
+        posts_filenames.sort(key=to_tuple)
+        last_file = posts_filenames[0]
+
+        post = {}
+        with open(os.path.join(students_dir, zid, last_file)) as f:
+            for line in f:
+                if ': ' in line:
+                    index = line.index(":")
+                    post[line[:index]] = line[index + 1:].strip()
+
+        comments = []
+        for i in range(1, len(posts_filenames)):
+            length = len(to_tuple(posts_filenames[i]))
+
+            if length == 2:
+                comment = {}
+                with open(os.path.join(students_dir, zid, posts_filenames[i])) as f:
+                    for line in f:
+                        if ': ' in line:
+                            index = line.index(":")
+                            comment[line[:index]] = line[index + 1:].strip()
+                comment["replies"] = []
+                comments.append(comment)
+                # Whenever a file of length 2 appears, it means a new comment
+                last_file = posts_filenames[i]
+
+            if length == 3:
+                reply = {}
+                with open(os.path.join(students_dir, zid, posts_filenames[i])) as f:
+                    for line in f:
+                        if ': ' in line:
+                            index = line.index(":")
+                            reply[line[:index]] = line[index + 1:].strip()
+                comments[-1]["replies"].append(reply)
+
+        post["comments"] = comments
+        posts.append(post)
+
+    posts_information["posts"] = posts
+
+    return posts_information
+
+
+students.remove({})
+posts.remove({})
+
+student_ids = os.listdir(students_dir)
+
+# read the datasets and save the contents to mongodb 
+for student_id in student_ids:
+    if student_id[0] == "z":
+        student_information = read_student_information(student_id)
+        students.insert(student_information)
+
+        student_posts = read_student_posts(student_id)
+        posts.insert(student_posts)
