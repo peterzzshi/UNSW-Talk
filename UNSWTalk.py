46bd3070815666c80ff82d24d5dc1750f9a0a109
Peter Shi <peterzzshi@gmail.com>
2018-11-29 00:18:58 +1100

    restore lib
    pymongo

--- UNSWTalk.py
+++ UNSWTalk.py
@@ -1,5 +1,10 @@
 from app import app
 
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
+
 # from flask import Flask, render_template, session, request, flash, redirect, url_for
 # from pymongo import MongoClient
 #
